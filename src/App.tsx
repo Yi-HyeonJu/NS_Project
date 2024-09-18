5be47276d2c793d69ddf1f05dd782d7f06950703
Yi-HyeonJu <guswwn1925@gmail.com>
2024-09-18 17:42:44 +0900

    feat: 기본 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Calendar from './pages/Calendar';
+import Home from './pages/Home';
+import List from './pages/List';
+
 function App() {
-  return <h1 className='p-2 text-center'>Home</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/list' element={<List />} />
+        <Route path='/calendar' element={<Calendar />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
