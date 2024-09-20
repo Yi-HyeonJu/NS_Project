9ef9a3eb2dba7d0392861a31dbc681b3377ab98a
Yi-HyeonJu <guswwn1925@gmail.com>
2024-09-20 21:58:43 +0900

    feat: 근무표 확인 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import List from './pages/List';
+import Schedule from './pages/Schedule';
 
 const App = () => {
   return (
@@ -14,10 +15,14 @@ const App = () => {
           <Link to='/list' className='mr-4'>
             인원 목록
           </Link>
+          <Link to='/schedule' className='mr-4'>
+            근무표
+          </Link>
         </nav>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/list' element={<List />} />
+          <Route path='/schedule' element={<Schedule />} />
         </Routes>
       </div>
     </Router>
