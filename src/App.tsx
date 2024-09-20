88f39043924a4c4a85b176a898ac5b5135eaebf7
Yi-HyeonJu <guswwn1925@gmail.com>
2024-09-20 16:34:43 +0900

    feat: 레이아웃 네브 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import Calendar from './pages/Calendar';
 import Home from './pages/Home';
 import List from './pages/List';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/list' element={<List />} />
-        <Route path='/calendar' element={<Calendar />} />
-      </Routes>
+      <div>
+        <nav className='p-4'>
+          <Link to='/' className='mr-4'>
+            인원 추가하기
+          </Link>
+          <Link to='/list' className='mr-4'>
+            인원 목록
+          </Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/list' element={<List />} />
+        </Routes>
+      </div>
     </Router>
   );
-}
+};
 
 export default App;
