9a29fb2f357382fc086898cb2a61707d3ecf4163
Yi-HyeonJu <guswwn1925@gmail.com>
2024-09-23 20:32:11 +0900

    feat: 근무표 보여줄 컴포넌트

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -2,6 +2,14 @@ const Schedule = () => {
   return (
     <div className='flex flex-col items-center justify-center gap-5 py-4'>
       <h2 className='text-[30px] font-semibold'>근무표</h2>
+      <form>
+        <button
+          type='submit'
+          className='px-2 py-1 bg-red-400 rounded-lg hover:bg-red-500'
+        >
+          근무표 만들기
+        </button>
+      </form>
     </div>
   );
 };
