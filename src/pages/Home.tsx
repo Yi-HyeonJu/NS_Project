fe3984d4b5d331ae2ed0dbd840b853215137bd42
Yi-HyeonJu <guswwn1925@gmail.com>
2024-09-25 17:15:20 +0900

    remove: 불필요한 변수 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,13 @@
 import ScheduleForm from '../components/ScheduleForm';
-import { useState } from 'react';
 import useStore from '../store/useStore';
 
 const Home = () => {
   const { addSchedule, setMonth } = useStore();
-  const [selectedDates, setSelectedDates] = useState<Date[]>([]);
 
   return (
     <div className='flex flex-col items-center justify-center gap-5 py-4'>
       <h2 className='text-[30px] font-semibold'>근무 인원 추가하기</h2>
-      <ScheduleForm
-        selectedDates={selectedDates}
-        setSelectedDates={setSelectedDates}
-        addSchedule={addSchedule}
-        setMonth={setMonth}
-      />
+      <ScheduleForm addSchedule={addSchedule} setMonth={setMonth} />
     </div>
   );
 };
